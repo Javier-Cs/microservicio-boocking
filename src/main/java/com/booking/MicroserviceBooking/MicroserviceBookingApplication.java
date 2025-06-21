08eec35b598413cacd569c71db776f379724d879
Javier-Cs <krchipullajavier@gmail.com>
2025-06-20 19:32:38 -0500

    Implementación de eurekaCliente en el servicioProduct - Implementación de la dependencia spring-cloud-starter-netflix-eureka-client - Implementación de la notación @EnableDiscoveryClient en la clase principal - llaves de configuración para eureka

--- src/main/java/com/booking/MicroserviceBooking/MicroserviceBookingApplication.java
+++ src/main/java/com/booking/MicroserviceBooking/MicroserviceBookingApplication.java
@@ -2,8 +2,10 @@ package com.booking.MicroserviceBooking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MicroserviceBookingApplication {
 
 	public static void main(String[] args) {
